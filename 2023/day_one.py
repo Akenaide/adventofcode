f62b5563fe33895759a77f49e1f755d834e38813
Kevin MS <kevinms1@gmail.com>
2024-02-15 13:04:53 +0000

    finished day 1 part 2

--- 2023/day_one.py
+++ 2023/day_one.py
@@ -1,18 +1,62 @@
 import re
 import pathlib
 
+translation_table = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
+letter_digit = re.compile(r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))")
+
 only_digit = re.compile(r"\d")
+errors = [
+    55205,
+    54992,
+    55130,
+    54980,
+]
+good = 54985
+
+
+def translate_to_digit(entry: str) -> str:
+    results = ""
+    matches = letter_digit.finditer(entry)
+    for d in [match.group(1) for match in matches]:
+        if not d.isnumeric():
+            results += translation_table[d]
+        else:
+            results += d
+
+    return results
+
+
 def get_digits(entry: str) -> str:
-    digits = only_digit.findall(entry)
+
+    digits = translate_to_digit(entry)
     return digits[0] + digits[-1]
 
+
 def main():
-    sum = 0
-    
-    f = open(pathlib.Path.joinpath(pathlib.Path(__file__).parent.resolve(), "input_day_one.txt" ))
-    for line in f.readlines():
-        sum += int(get_digits(line))
-    print(sum)
+    total = 0
+
+    f = open(
+        pathlib.Path.joinpath(
+            pathlib.Path(__file__).parent.resolve(), "input_day_one.txt"
+        )
+    )
+
+    total = sum(int(get_digits(line.strip())) for line in f.readlines())
+    if total != good:
+        print("Not good")
+    else:
+        print(total)
+
 
 if __name__ == "__main__":
     main()
--- 2023/test_day_one.py
+++ 2023/test_day_one.py
@@ -1,19 +1,93 @@
-import pytest 
+import pytest
 
 import day_one
+
 
 def test_first_last():
     entry = "1abc2"
     assert day_one.get_digits(entry) == "12"
-    
+
+
 def test_inside():
     entry = "pqr3stu8vwx"
     assert day_one.get_digits(entry) == "38"
+
 
 def test_many():
     entry = "a1b2c3d4e5f"
     assert day_one.get_digits(entry) == "15"
 
+
 def test_one():
     entry = "treb7uchet"
     assert day_one.get_digits(entry) == "77"
+
+
+def test_part2_2_letter():
+    entry = "two1nine"
+    assert day_one.get_digits(entry) == "29"
+
+
+def test_part2_3_letter():
+    entry = "eightwothree"
+    assert day_one.get_digits(entry) == "83"
+
+
+def test_part2_digit_letter():
+    entry = "abcone2threexyz"
+    assert day_one.get_digits(entry) == "13"
+
+
+def test_part2_digit_letter2():
+    entry = "xtwone3four"
+    assert day_one.get_digits(entry) == "24"
+
+
+def test_part2_digit():
+    entry = "4nineeightseven2"
+    assert day_one.get_digits(entry) == "42"
+
+
+def test_part2_letters_many_digit():
+    entry = "zoneight234"
+    assert day_one.get_digits(entry) == "14"
+
+
+def test_part2_digit_sixteen():
+    entry = "7pqrstsixteen"
+    assert day_one.get_digits(entry) == "76"
+
+
+def test_use_1():
+    entry = "ghthreetwo1mhgtklfqdkqthree7seventwone"
+    assert day_one.get_digits(entry) == "31"
+
+
+def test_use_2():
+    entry = "ghthreetwo1mhgtklfqdkqthree7seventwonethree"
+    assert day_one.get_digits(entry) == "33"
+
+
+def test_one_letter():
+    entry = "one"
+    assert day_one.get_digits(entry) == "11"
+
+
+def test_use_3():
+    entry = "oneoneoneone2"
+    assert day_one.get_digits(entry) == "12"
+
+
+def test_use_4():
+    entry = "nineghthreetwo1mhgtklfqdkqthree7seventwone"
+    assert day_one.get_digits(entry) == "91"
+
+
+def test_use_5():
+    entry = "s8twoned"
+    assert day_one.get_digits(entry) == "81"
+
+
+def test_use_6():
+    entry = "stwoned8"
+    assert day_one.get_digits(entry) == "28"
